c9ef1ebebbf5d55c3e6635813aa33b6df6866a1c
Cameron Dutro <camertron@gmail.com>
2015-04-15 18:50:20 -0700

    Replace Rails 4 dummy app with Rails 3

--- comply.gemspec
+++ comply.gemspec
@@ -19,8 +19,6 @@ Gem::Specification.new do |s|
   s.add_dependency 'rails', '~> 3.2.0'
   s.add_dependency 'strong_parameters', '~> 0.2.0'
 
-  s.add_development_dependency 'jasmine-rails'
-  s.add_development_dependency 'sinon-rails'
   s.add_development_dependency 'pry-nav'
   s.add_development_dependency 'rspec-rails', '~> 3.2.0'
   s.add_development_dependency 'sqlite3'
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -9,17 +9,17 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513043637) do
+ActiveRecord::Schema.define(:version => 20140513043637) do
 
-  create_table "movies", force: true do |t|
+  create_table "movies", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.date     "release_date"
     t.integer  "rating"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
