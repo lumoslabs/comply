d0b3812e6ba7d73fcec1253ad15d5aa3ee365529
abMatGit <amatuszewski@lumoslabs.com>
2016-07-11 17:09:24 -0700

    remove ssl enforcement

--- app/controllers/comply/application_controller.rb
+++ app/controllers/comply/application_controller.rb
@@ -1,11 +1,4 @@
 module Comply
   class ApplicationController < ActionController::Base
-    force_ssl if: :ssl_configured?
-
-    private
-
-    def ssl_configured?
-      Rails.env.staging? || Rails.env.production?
-    end
   end
 end
