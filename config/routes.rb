3d5f6da463ddde5af6bb0afe161e3a4feb096d14
Logan McGrath <logan@lumoslabs.com>
2016-10-10 10:32:13 -0700

    Ensuring both GET and POST work for validation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Comply::Engine.routes.draw do
-  match '/validations' => 'validations#show', via: [:post]
+  match '/validations' => 'validations#show', via: [:get, :post]
   # allow CORS requests
   match 'validations', via: [:options], to: proc { [200, {}, ['']] }
 end
--- lib/comply/version.rb
+++ lib/comply/version.rb
@@ -1,3 +1,3 @@
 module Comply
-  VERSION = '1.8.1.beta'
+  VERSION = '1.8.1.beta2'
 end
